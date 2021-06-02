30894110f3d2f22ebb76877c09e49fedb07040ab
patrycjarecko <73533460+patrycjarecko@users.noreply.github.com>
2021-06-02 12:08:03 +0200

    create

--- main.c
+++ main.c
@@ -2,19 +2,27 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+
 int main(){
 
-int N=10;
-#tablica watkow
+    int N=10;
+    pthread_t threads[N];
 
-pthread ph 
-#tworzenie n wątkow
-for(int i=1; i<= N, i++){
-    pthread.create()
-}
+    for(int i=0; i<= N, ++i){
+        if(pthread_create(&threads[i], NULL, fun, NULL)!=0){
+            printf("błąd");
+            exit(-1);
+        }
+    }
+
+
+    for (int i=0; i<= N, ++i) {
+        pthread_join(threads[i], NULL);
+    }
+
 
 #funkcje A B
 
 #semafor i mutex na most
-
+    return 0;
 }
