2683ca2aa7bfa62d402db150aed56db4065c3b2f
Sawi <61832069+sawica@users.noreply.github.com>
2021-06-02 11:22:36 +0200

    Update and rename main to main.c

--- main.c
+++ main.c
@@ -4,9 +4,17 @@
 
 int main(){
 
+int N=10;
 #tablica watkow
-#twozrenie n wątkow
+
+pthread ph 
+#tworzenie n wątkow
+for(int i=1; i<= N, i++){
+    pthread.create()
+}
+
 #funkcje A B
+
 #semafor i mutex na most
 
 }
